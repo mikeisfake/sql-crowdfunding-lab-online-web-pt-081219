57cd8858bba9e7a3f531775b19cdb6c1a8371819
Mike Cooper <mikescooper@icloud.com>
2019-10-23 04:10:57 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,8 +24,8 @@ end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   "SELECT users.name, SUM(pledges.amount)
-  FROM users ON pledges.id 
-  JOIN pledges "
+  FROM users 
+  JOIN pledges ON users.id = pledges.user_id"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
