26725ca2cfdea35c1c6015c1192cbe679b3d020b
Mike Cooper <mikescooper@icloud.com>
2019-10-23 04:19:48 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -33,6 +33,7 @@ end
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   "SELECT projects.category, SUM(pledges.amount)
   FROM projects
+  WHERE projects.category = 'music'
   JOIN pledges ON projects.id = pledges.project_id;"
 end
 
